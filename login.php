4d3098be10dd9e00420ef614f0e84cda29162404
3XKINGX3 <92936628+3XKINGX3@users.noreply.github.com>
2026-04-19 23:17:10 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+$pdo = new PDO(
+  'mysql:host=localhost;dbname=YOUR_DB;charset=utf8',
+  'YOUR_USER',
+  'YOUR_PASS'
+);
+
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+
+  $login=$_POST['login'];
+  $pass=$_POST['password'];
+
+  $stmt=$pdo->prepare("SELECT * FROM applications WHERE login=?");
+  $stmt->execute([$login]);
+  $user=$stmt->fetch();
+
+  if ($user && password_verify($pass,$user['password_hash'])) {
+    $_SESSION['user_id']=$user['id'];
+    header("Location: index.php");
+    exit();
+  }
+
+  echo "Ошибка входа";
+}
+?>
+
+<form method="POST">
+<input name="login" placeholder="логин">
+<input name="password" type="password" placeholder="пароль">
+<button>Войти</button>
+</form>
